836680fb52ab5e28c0fe9d16195f884a08ae3f4e
Alexandre Lara <allexandrelara@gmail.com>
2025-04-04 17:27:07 -0300

    feat: Update data processing functions and improve documentation
    - Renamed the dataset from 'kobe_shots_dev' to 'raw_kobe_shots_dev' in the data preparation notebook and processing nodes.
    - Enhanced the `preprocess_kobe_shots` function to convert 'shot_made_flag' to 'Int8' data type.
    - Updated function documentation to reflect changes in preprocessing steps and expected outputs.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_processing/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_processing/nodes.py
@@ -6,6 +6,9 @@ import pandas as pd
 
 
 def _rename_columns(df):
+    """
+    Rename columns to match the expected format for the model.
+    """
     df = df.rename(columns={
         'lon': 'lng',
     })
@@ -19,14 +22,29 @@ def _filter_columns(df: pd.DataFrame) -> pd.DataFrame:
 
 def preprocess_kobe_shots(kobe_shots: pd.DataFrame) -> pd.DataFrame:
     """
-    Preprocess the Kobe shots DataFrame by renaming columns.
+    Preprocess the Kobe shots DataFrame by renaming columns and converting data types.
+
+    The output dataframe is saved in the intermediate layer.
+
+    Args:
+        kobe_shots (pd.DataFrame): The raw Kobe shots DataFrame.
+    Returns:
+        pd.DataFrame: The preprocessed Kobe shots DataFrame.
     """
     kobe_shots = _rename_columns(kobe_shots)
+    kobe_shots['shot_made_flag'] = kobe_shots['shot_made_flag'].astype('Int8')
     return kobe_shots
 
 def create_model_input_table(kobe_shots: pd.DataFrame) -> pd.DataFrame:
     """
     Create the model input table by parsing, filtering and transforming the data.
+
+    The output is saved in the primary layer.
+
+    Args:
+        kobe_shots (pd.DataFrame): The preprocessed Kobe shots DataFrame.
+    Returns:
+        pd.DataFrame: The model input table.
     """
     model_input_table = _filter_columns(kobe_shots)
     model_input_table = model_input_table.dropna()
