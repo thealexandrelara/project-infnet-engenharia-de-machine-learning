55008769f712e72898c026312309bac5fd1dee22
Alexandre Lara <allexandrelara@gmail.com>
2025-04-06 17:45:15 -0300

    feat: Add data splitting functionality to the data science pipeline
    - Introduced a new `split_data` function to split the dataset into training and testing sets using PyCaret.
    - Updated the pipeline to include the new node for data splitting, linking it to the model input table and parameters.
    - Enhanced configuration files to define model options for training and testing datasets.
    - Added new dataset entries in the catalog for the training and testing data outputs.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
@@ -2,3 +2,39 @@
 This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.19.12
 """
+
+import pandas as pd
+from pycaret.classification import ClassificationExperiment
+
+
+def split_data(
+    df: pd.DataFrame,
+    parameters: dict
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split the DataFrame into training and testing sets.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame to be split.
+        train_size (float): The proportion of the dataset to include in the training set.
+        random_state (int): Random seed for reproducibility.
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame]: A tuple containing the training and testing DataFrames.
+    """
+    experiment = ClassificationExperiment()
+    experiment.setup(
+        data=df,
+        target=parameters['target'],
+        train_size=parameters['train_size'],
+        session_id=parameters['random_state'],
+        data_split_stratify=True,
+        data_split_shuffle=True,
+    )
+    X_train, X_test = experiment.get_config('X_train'), experiment.get_config('X_test')
+    y_train, y_test = experiment.get_config('y_train'), experiment.get_config('y_test')
+
+    train_data = pd.concat([X_train, y_train], axis=1)
+    test_data = pd.concat([X_test, y_test], axis=1)
+
+    return train_data, test_data
--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
@@ -4,7 +4,15 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from .nodes import split_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=split_data,
+            inputs=["model_input_table", "params:model_options"],
+            outputs=["base_train", "base_test"],
+            name="split_data_node",
+        )
+    ])
