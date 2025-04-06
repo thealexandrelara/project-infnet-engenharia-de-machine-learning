bd6849afc32e26143158a8513d0698756ed778fe
Alexandre Lara <allexandrelara@gmail.com>
2025-04-06 17:45:24 -0300

    feat: Add logistic regression and decision tree model training functions
    - Introduced `train_logistic_regression_model` and `train_decision_tree_model` functions to facilitate model training within the data science pipeline.
    - Each function includes setup for the `ClassificationExperiment` and accepts training data and parameters for model configuration.
    - Enhanced documentation for both functions to clarify input arguments and return types.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
@@ -38,3 +38,51 @@ def split_data(
     test_data = pd.concat([X_test, y_test], axis=1)
 
     return train_data, test_data
+
+def train_logistic_regression_model(
+    train_data: pd.DataFrame,
+    parameters: dict
+) -> tuple[ClassificationExperiment, pd.DataFrame]:
+    """
+    Train a logistic regression model using the provided training data.
+
+    Args:
+        train_data (pd.DataFrame): The training data.
+        parameters (dict): Parameters for the model.
+
+    Returns:
+        tuple[ClassificationExperiment, pd.DataFrame]: A tuple containing the trained experiment and the test data.
+    """
+    experiment = ClassificationExperiment()
+    experiment.setup(
+        data=train_data,
+        target=parameters['target'],
+        train_size=1.0,
+        session_id=parameters['random_state'],
+    )
+    model = experiment.create_model('lr')
+
+    return experiment
+
+def train_decision_tree_model(
+    train_data: pd.DataFrame,
+    parameters: dict
+) -> tuple[ClassificationExperiment, pd.DataFrame]:
+    """
+    Train a decision tree model using the provided training data.
+    Args:
+        train_data (pd.DataFrame): The training data.
+        parameters (dict): Parameters for the model.
+    Returns:
+        tuple[ClassificationExperiment, pd.DataFrame]: A tuple containing the trained experiment and the test data.
+    """
+    experiment = ClassificationExperiment()
+    experiment.setup(
+        data=train_data,
+        target=parameters['target'],
+        train_size=1.0,
+        session_id=parameters['random_state'],
+    )
+    model = experiment.create_model('dt')
+
+    return experiment
