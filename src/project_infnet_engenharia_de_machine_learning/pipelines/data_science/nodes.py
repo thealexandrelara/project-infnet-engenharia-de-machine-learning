5f70d5f0a9cd22b734fe65f77be5f43e0a1c61e7
Alexandre Lara <allexandrelara@gmail.com>
2025-04-07 18:42:27 -0300

    feat: Enhance model training and evaluation in the data science pipeline
    - Updated `pyproject.toml` to specify version constraints for `pyarrow` and `kedro-mlflow`.
    - Added new nodes for plotting AUC curves of logistic regression and decision tree models in the pipeline.
    - Introduced metrics for F1 score and log loss in the training functions for both models.
    - Enhanced the catalog and parameters configuration to support new model artifacts and filenames.
    - Refactored the training functions to utilize a helper function for setting up classification experiments.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
@@ -3,14 +3,37 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.19.12
 """
 
+import os
+
 import pandas as pd
 from kedro_mlflow.io.metrics import MlflowMetricDataset
+from PIL import Image
 from pycaret.classification import ClassificationExperiment
 from sklearn.metrics import f1_score, log_loss
 
 logistic_regression_log_loss_metric = MlflowMetricDataset(key="logistic_regression_model_log_loss")
+logistic_regression_f1_score_metric = MlflowMetricDataset(key="logistic_regression_model_f1_score")
 decision_tree_log_loss_metric = MlflowMetricDataset(key="decision_tree_model_log_loss")
 decision_tree_f1_score_metric = MlflowMetricDataset(key="decision_tree_model_f1_score")
+
+def _createClassificationExperiment(
+    train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
+    parameters: dict
+) -> ClassificationExperiment:
+    experiment = ClassificationExperiment()
+    experiment.setup(
+        data=train_data,
+        test_data=test_data,
+        target=parameters['target'],
+        session_id=parameters['random_state'],
+        log_data=True,
+        log_plots=True,
+        experiment_name='project_infnet_engenharia_de_machine_learning',
+    )
+    experiment.add_metric('logloss', 'Log Loss', log_loss, greater_is_better=False)
+
+    return experiment
 
 
 def train_logistic_regression_model(
@@ -28,19 +51,15 @@ def train_logistic_regression_model(
     Returns:
         tuple[ClassificationExperiment, pd.DataFrame]: A tuple containing the trained experiment and the test data.
     """
-    experiment = ClassificationExperiment()
-    experiment.setup(
-        data=train_data,
-        test_data=test_data,
-        target=parameters['target'],
-        session_id=parameters['random_state'],
-    )
+    experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
     logistic_regression_model = experiment.create_model('lr')
     logistic_regression_predictions= experiment.predict_model(logistic_regression_model, data=test_data)
     logistic_regression_log_loss = log_loss(test_data[parameters['target']], logistic_regression_predictions["prediction_score"])
+    logistic_regression_f1_score = f1_score(test_data[parameters['target']], logistic_regression_predictions["prediction_label"])
+    logistic_regression_f1_score_metric.save(logistic_regression_f1_score)
     logistic_regression_log_loss_metric.save(logistic_regression_log_loss)
 
-    return experiment
+    return logistic_regression_model
 
 def train_decision_tree_model(
     train_data: pd.DataFrame,
@@ -55,13 +74,7 @@ def train_decision_tree_model(
     Returns:
         tuple[ClassificationExperiment, pd.DataFrame]: A tuple containing the trained experiment and the test data.
     """
-    experiment = ClassificationExperiment()
-    experiment.setup(
-        data=train_data,
-        test_data=test_data,
-        target=parameters['target'],
-        session_id=parameters['random_state'],
-    )
+    experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
     decision_tree_model = experiment.create_model('dt')
     decision_tree_predictions= experiment.predict_model(decision_tree_model, data=test_data)
     decision_tree_log_loss = log_loss(test_data[parameters['target']], decision_tree_predictions["prediction_score"])
@@ -69,4 +82,26 @@ def train_decision_tree_model(
     decision_tree_log_loss_metric.save(decision_tree_log_loss)
     decision_tree_f1_score_metric.save(decision_tree_f1_score)
 
-    return experiment
+    return decision_tree_model
+
+def plot_auc(
+    train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
+    parameters: dict,
+    model,
+    output_path: str,
+):
+    """
+    Plot the AUC curve for the given model.
+    Args:
+        model: The trained model.
+    """
+    experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
+    saved_image_path = experiment.plot_model(model, plot='auc', save=True)
+
+    if os.path.exists(saved_image_path):
+        os.replace(saved_image_path, output_path)
+
+    with Image.open(output_path) as img:
+        img.load()
+        return img.copy()
--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from .nodes import train_logistic_regression_model, train_decision_tree_model
+from .nodes import train_logistic_regression_model, train_decision_tree_model, plot_auc
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -12,13 +12,25 @@ def create_pipeline(**kwargs) -> Pipeline:
         node(
             func=train_logistic_regression_model,
             inputs=["base_train", "base_test", "params:model_options"],
-            outputs=None,
+            outputs='logistic_regression_model',
             name="train_logistic_regression_model_node",
         ),
         node(
             func=train_decision_tree_model,
             inputs=["base_train", "base_test", "params:model_options"],
-            outputs=None,
+            outputs='decision_tree_model',
             name="train_decision_tree_model_node",
+        ),
+        node(
+            func=plot_auc,
+            inputs=["base_train", "base_test", "params:model_options", "logistic_regression_model", "params:logistic_regression_model_auc_filename_path"],
+            outputs='logistic_regression_model_auc',
+            name="plot_logistic_regression_model_auc_node",
+        ),
+        node(
+            func=plot_auc,
+            inputs=["base_train", "base_test", "params:model_options", "decision_tree_model", "params:decision_tree_model_auc_filename_path"],
+            outputs='decision_tree_model_auc',
+            name="plot_decision_tree_model_auc_node",
         )
     ])
