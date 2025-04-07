28a8c6c59b8cbf3803613046eadf59c88480ab40
Alexandre Lara <allexandrelara@gmail.com>
2025-04-06 21:36:47 -0300

    feat: Enhance model training with scikit-learn integration and metrics
    - Added `scikit-learn` as a dependency in `pyproject.toml`, `requirements.txt`, and `uv.lock` for model evaluation.
    - Updated `train_logistic_regression_model` and `train_decision_tree_model` functions to include test data and calculate log loss and F1 score metrics.
    - Integrated `MlflowMetricDataset` for saving model performance metrics in the data science pipeline.
    - Updated the pipeline to include new nodes for training both logistic regression and decision tree models.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
@@ -4,11 +4,18 @@ generated using Kedro 0.19.12
 """
 
 import pandas as pd
+from kedro_mlflow.io.metrics import MlflowMetricDataset
 from pycaret.classification import ClassificationExperiment
+from sklearn.metrics import f1_score, log_loss
+
+logistic_regression_log_loss_metric = MlflowMetricDataset(key="logistic_regression_model_log_loss")
+decision_tree_log_loss_metric = MlflowMetricDataset(key="decision_tree_model_log_loss")
+decision_tree_f1_score_metric = MlflowMetricDataset(key="decision_tree_model_f1_score")
 
 
 def train_logistic_regression_model(
     train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
     parameters: dict
 ) -> tuple[ClassificationExperiment, pd.DataFrame]:
     """
@@ -24,16 +31,20 @@ def train_logistic_regression_model(
     experiment = ClassificationExperiment()
     experiment.setup(
         data=train_data,
+        test_data=test_data,
         target=parameters['target'],
-        train_size=1.0,
         session_id=parameters['random_state'],
     )
-    model = experiment.create_model('lr')
+    logistic_regression_model = experiment.create_model('lr')
+    logistic_regression_predictions= experiment.predict_model(logistic_regression_model, data=test_data)
+    logistic_regression_log_loss = log_loss(test_data[parameters['target']], logistic_regression_predictions["prediction_score"])
+    logistic_regression_log_loss_metric.save(logistic_regression_log_loss)
 
     return experiment
 
 def train_decision_tree_model(
     train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
     parameters: dict
 ) -> tuple[ClassificationExperiment, pd.DataFrame]:
     """
@@ -47,10 +58,15 @@ def train_decision_tree_model(
     experiment = ClassificationExperiment()
     experiment.setup(
         data=train_data,
+        test_data=test_data,
         target=parameters['target'],
-        train_size=1.0,
         session_id=parameters['random_state'],
     )
-    model = experiment.create_model('dt')
+    decision_tree_model = experiment.create_model('dt')
+    decision_tree_predictions= experiment.predict_model(decision_tree_model, data=test_data)
+    decision_tree_log_loss = log_loss(test_data[parameters['target']], decision_tree_predictions["prediction_score"])
+    decision_tree_f1_score = f1_score(test_data[parameters['target']], decision_tree_predictions["prediction_label"])
+    decision_tree_log_loss_metric.save(decision_tree_log_loss)
+    decision_tree_f1_score_metric.save(decision_tree_f1_score)
 
     return experiment
--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
@@ -4,9 +4,21 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from .nodes import train_logistic_regression_model, train_decision_tree_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-
+        node(
+            func=train_logistic_regression_model,
+            inputs=["base_train", "base_test", "params:model_options"],
+            outputs=None,
+            name="train_logistic_regression_model_node",
+        ),
+        node(
+            func=train_decision_tree_model,
+            inputs=["base_train", "base_test", "params:model_options"],
+            outputs=None,
+            name="train_decision_tree_model_node",
+        )
     ])
