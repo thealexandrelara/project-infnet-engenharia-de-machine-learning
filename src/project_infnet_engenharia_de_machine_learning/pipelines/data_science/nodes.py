d550e090745bfcfe07437c7a68d96b7451123641
Alexandre Lara <allexandrelara@gmail.com>
2025-04-08 11:19:16 -0300

    feat: Add confusion matrix and feature importance plots for logistic regression and decision tree models
    - Introduced new nodes to save confusion matrix and feature importance plots in the data science pipeline.
    - Updated catalog and parameters configuration to include paths for the new artifacts.
    - Refactored existing plotting functions to streamline image saving process.

--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/nodes.py
@@ -9,7 +9,7 @@ import pandas as pd
 from kedro_mlflow.io.metrics import MlflowMetricDataset
 from PIL import Image
 from pycaret.classification import ClassificationExperiment
-from sklearn.metrics import f1_score, log_loss
+from sklearn.metrics import log_loss
 
 logistic_regression_log_loss_metric = MlflowMetricDataset(key="logistic_regression_model_log_loss")
 logistic_regression_f1_score_metric = MlflowMetricDataset(key="logistic_regression_model_f1_score")
@@ -35,6 +35,26 @@ def _createClassificationExperiment(
 
     return experiment
 
+def _save_plot_image_to_reporting_folder(
+    plot_name: str,
+    experiment: ClassificationExperiment,
+    model,
+    output_path: str,
+):
+    """
+    Save the image to the specified output path.
+    Args:
+        image (Image): The image to save.
+        output_path (str): The path where the image will be saved.
+    """
+    saved_image_path = experiment.plot_model(model, plot=plot_name, save=True)
+
+    if os.path.exists(saved_image_path):
+        os.replace(saved_image_path, output_path)
+
+    with Image.open(output_path) as img:
+        img.load()
+        return img.copy()
 
 def train_logistic_regression_model(
     train_data: pd.DataFrame,
@@ -53,11 +73,11 @@ def train_logistic_regression_model(
     """
     experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
     logistic_regression_model = experiment.create_model('lr')
-    logistic_regression_predictions= experiment.predict_model(logistic_regression_model, data=test_data)
-    logistic_regression_log_loss = log_loss(test_data[parameters['target']], logistic_regression_predictions["prediction_score"])
-    logistic_regression_f1_score = f1_score(test_data[parameters['target']], logistic_regression_predictions["prediction_label"])
-    logistic_regression_f1_score_metric.save(logistic_regression_f1_score)
-    logistic_regression_log_loss_metric.save(logistic_regression_log_loss)
+    scoring_grid = experiment.pull()
+    log_loss_score = scoring_grid['Log Loss'][0]
+    f1_score = scoring_grid['F1'][0]
+    logistic_regression_f1_score_metric.save(log_loss_score)
+    logistic_regression_log_loss_metric.save(f1_score)
 
     return logistic_regression_model
 
@@ -76,15 +96,15 @@ def train_decision_tree_model(
     """
     experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
     decision_tree_model = experiment.create_model('dt')
-    decision_tree_predictions= experiment.predict_model(decision_tree_model, data=test_data)
-    decision_tree_log_loss = log_loss(test_data[parameters['target']], decision_tree_predictions["prediction_score"])
-    decision_tree_f1_score = f1_score(test_data[parameters['target']], decision_tree_predictions["prediction_label"])
-    decision_tree_log_loss_metric.save(decision_tree_log_loss)
-    decision_tree_f1_score_metric.save(decision_tree_f1_score)
+    scoring_grid = experiment.pull()
+    log_loss_score = scoring_grid['Log Loss'][0]
+    f1_score = scoring_grid['F1'][0]
+    decision_tree_log_loss_metric.save(log_loss_score)
+    decision_tree_f1_score_metric.save(f1_score)
 
     return decision_tree_model
 
-def plot_auc(
+def save_auc_plot(
     train_data: pd.DataFrame,
     test_data: pd.DataFrame,
     parameters: dict,
@@ -97,11 +117,53 @@ def plot_auc(
         model: The trained model.
     """
     experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
-    saved_image_path = experiment.plot_model(model, plot='auc', save=True)
+    return _save_plot_image_to_reporting_folder(
+        plot_name='auc',
+        experiment=experiment,
+        model=model,
+        output_path=output_path,
+    )
 
-    if os.path.exists(saved_image_path):
-        os.replace(saved_image_path, output_path)
+def save_confusion_matrix_plot(
+    train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
+    parameters: dict,
+    model,
+    output_path: str,
+):
+    """
+    Plot the Confusion Matrix for the given model.
+    Args:
+        model: The trained model.
+    Returns:
+        Image: The confusion matrix image.
+    """
+    experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
+    return _save_plot_image_to_reporting_folder(
+        plot_name='confusion_matrix',
+        experiment=experiment,
+        model=model,
+        output_path=output_path,
+    )
 
-    with Image.open(output_path) as img:
-        img.load()
-        return img.copy()
+def save_feature_importance_plot(
+    train_data: pd.DataFrame,
+    test_data: pd.DataFrame,
+    parameters: dict,
+    model,
+    output_path: str,
+):
+    """
+    Plot the Confusion Matrix for the given model.
+    Args:
+        model: The trained model.
+    Returns:
+        Image: The confusion matrix image.
+    """
+    experiment = _createClassificationExperiment(train_data=train_data, test_data=test_data, parameters=parameters)
+    return _save_plot_image_to_reporting_folder(
+        plot_name='feature',
+        experiment=experiment,
+        model=model,
+        output_path=output_path,
+    )
--- src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
+++ src/project_infnet_engenharia_de_machine_learning/pipelines/data_science/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from .nodes import train_logistic_regression_model, train_decision_tree_model, plot_auc
+from .nodes import train_logistic_regression_model, train_decision_tree_model, save_auc_plot, save_confusion_matrix_plot, save_feature_importance_plot
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -22,15 +22,39 @@ def create_pipeline(**kwargs) -> Pipeline:
             name="train_decision_tree_model_node",
         ),
         node(
-            func=plot_auc,
+            func=save_auc_plot,
             inputs=["base_train", "base_test", "params:model_options", "logistic_regression_model", "params:logistic_regression_model_auc_filename_path"],
             outputs='logistic_regression_model_auc',
-            name="plot_logistic_regression_model_auc_node",
+            name="save_logistic_regression_model_auc_plot_node",
         ),
         node(
-            func=plot_auc,
+            func=save_auc_plot,
             inputs=["base_train", "base_test", "params:model_options", "decision_tree_model", "params:decision_tree_model_auc_filename_path"],
             outputs='decision_tree_model_auc',
-            name="plot_decision_tree_model_auc_node",
+            name="save_decision_tree_model_auc_plot_node",
+        ),
+        node(
+            func=save_confusion_matrix_plot,
+            inputs=["base_train", "base_test", "params:model_options", "logistic_regression_model", "params:logistic_regression_model_confusion_matrix_filename_path"],
+            outputs='logistic_regression_model_confusion_matrix',
+            name="save_logistic_regression_model_confusion_matrix_plot_node",
+        ),
+        node(
+            func=save_confusion_matrix_plot,
+            inputs=["base_train", "base_test", "params:model_options", "decision_tree_model", "params:decision_tree_model_confusion_matrix_filename_path"],
+            outputs='decision_tree_model_confusion_matrix',
+            name="save_decision_tree_model_confusion_matrix_plot_node",
+        ),
+        node(
+            func=save_feature_importance_plot,
+            inputs=["base_train", "base_test", "params:model_options", "logistic_regression_model", "params:logistic_regression_model_feature_importance_filename_path"],
+            outputs='logistic_regression_model_feature_importance',
+            name="save_logistic_regression_model_feature_importance_plot_node",
+        ),
+        node(
+            func=save_feature_importance_plot,
+            inputs=["base_train", "base_test", "params:model_options", "decision_tree_model", "params:decision_tree_model_feature_importance_filename_path"],
+            outputs='decision_tree_model_feature_importance',
+            name="save_decision_tree_model_feature_importance_plot_node",
         )
     ])
